a78353474f58e9295754724ad16f09644723dc4a
AlexYuen31415 <alexyuen31415@gmail.com>
2026-02-21 09:56:15 +0000

    pig is actually latining this time

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -13,7 +13,17 @@ public class PigLatin {
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-        if (findFirstVowel(sWord) == 0) {return sWord + "way";}
-        else {return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0, findFirstVowel(sWord)) + "ay";}
+        int firstVowel = findFirstVowel(sWord);
+
+        if ((sWord.length() >= 2) && sWord.substring(0, 2).equals("qu")) {
+            return sWord.substring(2) + "quay";
+        }
+        if (firstVowel == -1) {
+            return sWord + "ay";
+        }
+        if (firstVowel == 0) {
+            return sWord + "way";
+        }
+        return sWord.substring(firstVowel) + sWord.substring(0, firstVowel) + "ay";
     }
 }
